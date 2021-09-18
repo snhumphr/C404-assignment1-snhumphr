20a3d7e00d47eeb3d9cdb24b0df9229ccd25ddd4
snhumphr <snhumphr@ualberta.ca>
2021-09-18 14:49:13 -0600

    Added handling of 404 errors and started working on accessing local files

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #  coding: utf-8 
 import socketserver
+from os import path
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -32,7 +33,46 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        
+        client_request = self.data.decode('utf-8').split(" ")
+        print("This is the client request: ", client_request)
+        
+        #TODO: handle requests other then GET here
+        
+        filepath = client_request[1]
+        
+        print("This is the filepath: \"" + filepath + "\"")
+        
+        status_line = self.get_status_line(filepath)
+            
+        self.request.sendall(bytearray(status_line,'utf-8'))
+        
+    def get_status_line(self, filepath):
+        found = self.check_valid(filepath)
+        
+        if not found:
+            #check if this can be fixed with a 301 redirect
+            pass
+            #if it can't, then throw a 404 error here
+            status = "404 Not Found"
+        else:
+            #handle properly
+            status = "200 OK"
+            
+        return "HTTP/1.1 " + status
+    
+        
+    def get_file(self, filepath):
+        filedata = None
+        f = open("./www/" + filepath, "r")
+        filedata = f.read()
+        return str(filedata)
+    
+    def check_valid(self, filepath):
+        if path.exists("./" + filepath):
+            return True
+        else:
+            return False 
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
